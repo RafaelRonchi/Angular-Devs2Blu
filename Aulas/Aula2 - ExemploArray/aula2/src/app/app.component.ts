44eb7e145a95a70068187223e3a2695342827464
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-09-18 15:55:01 -0300

    lista com filtros

--- Aulas/Aula2 - ExemploArray/aula2/src/app/app.component.ts
+++ Aulas/Aula2 - ExemploArray/aula2/src/app/app.component.ts
@@ -9,31 +9,33 @@ import { WishItem } from 'src/shared/models/wishItem';
 export class AppComponent {
   tipoInput = "checkbox"
   estiloBoot = "btn btn-primary"
+
   items : WishItem[] = [
-    // new WishItem('Coderbyte'),
-    // new WishItem('Docker'),
-    // new WishItem('Memoria Ram'),
-    // new WishItem('Sushi'),
-    // new WishItem('Java'),
-    // new WishItem('Php',true),
-    // new WishItem('Crepe de Linguiça com Queijo'),
-    // new WishItem('Bolacha Trakinas de Morango'),
-    // new WishItem('Dinheiro'),
-    // new WishItem('Gatos'),
-    // new WishItem('Emprego com c#'),
-    // new WishItem('Bolo de Murango'),
-    // new WishItem('Jujuba'),
-    // new WishItem('Laranjinha'),
-    // new WishItem('Cors Inexistente'),
-    // new WishItem('Viajar para o Japão'),
-    // new WishItem('RTX 4090'),
-    // new WishItem('Chá de Leite'),
-    // new WishItem('Paçoquinha',true),
-    // new WishItem('Sorvete'),
-    // new WishItem('Kombi azul calcinha com cortina amarela de flor <3')  
+    new WishItem('Coderbyte'),
+    new WishItem('Docker'),
+    new WishItem('Memoria Ram'),
+    new WishItem('Sushi'),
+    new WishItem('Java'),
+    new WishItem('Php',true),
+    new WishItem('Crepe de Linguiça com Queijo'),
+    new WishItem('Bolacha Trakinas de Morango'),
+    new WishItem('Dinheiro'),
+    new WishItem('Gatos'),
+    new WishItem('Emprego com c#'),
+    new WishItem('Bolo de Murango'),
+    new WishItem('Jujuba'),
+    new WishItem('Laranjinha'),
+    new WishItem('Cors Inexistente'),
+    new WishItem('Viajar para o Japão'),
+    new WishItem('RTX 4090'),
+    new WishItem('Chá de Leite'),
+    new WishItem('Paçoquinha',true),
+    new WishItem('Sorvete'),
+    new WishItem('Kombi azul calcinha com cortina amarela de flor <3')
   ]
   title = 'Minha Lista de Desejos';
   newWishText = ""
+  visibleItens : WishItem[] = this.items
 
   addNewWish(){
     this.items.push(new WishItem(this.newWishText))
@@ -41,6 +43,20 @@ export class AppComponent {
   }
   toggleItem(item:WishItem){
     item.isComplete = !item.isComplete
+  }
+
+  listFilter: String = ""
+  onFilterChange(value: any){
+    this.listFilter = value
+    if (value === '0'){
+      this.visibleItens = this.items
+    }
+    else if (value === '1'){
+      this.visibleItens = this.items.filter(item => !item.isComplete)
+    }
+    else{
+      this.visibleItens = this.items.filter(item => item.isComplete)
+    }
   }
 
 }
